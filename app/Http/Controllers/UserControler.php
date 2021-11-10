952f5070fdea6dda7ecb0f6450719f3252dbbe85
Vilmar Vieira <vilmarvieira281215@gmail.com>
2021-11-09 21:06:38 -0300

    Atividade 07

--- app/Http/Controllers/UserControler.php
+++ app/Http/Controllers/UserControler.php
@@ -4,10 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\product;
+
 class UserControler extends Controller
 {
     public function get()
     {
         return "Olá controller";
     }
+    public function index(){
+        $products = Product::all();
+        return view('welcome',['products'=> $products]);
+    }
 }
+
